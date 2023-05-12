7a611b5739a7e4523a040857a2b5f91034bf56b9
Everylisy <anashape710@gmail.com>
2023-05-13 00:03:26 +0900

    style: 라우터 생성 방식 변경, 로그인 상태에 따라 조건부 렌더링 추가

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Auth from "../pages/Auth";
+import Home from "../pages/Home";
+import type { routerProps } from "../types";
+
+const AppRouter = ({ isLoggedIn }: routerProps) => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={isLoggedIn ? <Home /> : <Auth />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export interface routerProps {
+  isLoggedIn: boolean;
+}
